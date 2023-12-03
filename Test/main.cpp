6ac191fea0543a4745adf4f1dfd8624c24129710
polina <forgotten01011998@gmail.com>
2023-12-03 21:43:16 +0300

    googletest working link to project

---
+++ Test/main.cpp
@@ -0,0 +1,17 @@
+
+#include <gtest/gtest.h>
+#include <Configjson.h>
+
+
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+
+    ConfigJson hui;
+
+    return RUN_ALL_TESTS();
+
+
+}
---
+++ Test/tst_cfg.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+using namespace testing;
+
+TEST(Project, cfg)
+{
+
+    EXPECT_EQ(1, 1);
+    ASSERT_THAT(0, Eq(0));
+}
+TEST(Project, test)
+{
+
+
+}
+
